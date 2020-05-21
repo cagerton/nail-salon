237387947e80690bd3b9eee83d733ffd4a03d17f
Chris Agerton <cagerton@gmail.com>
2020-05-20 20:59:29 -0700

    Formatting, linting, dependency updates

--- src/manicure.rs
+++ src/manicure.rs
@@ -1,19 +1,15 @@
-extern crate wasm_bindgen;
-extern crate cfg_if;
 #[macro_use]
 extern crate serde_derive;
 
 use cfg_if::cfg_if;
-use std::io::{self, Cursor, Read};
+use exif::{In, Tag};
+use image::imageops;
+use image::jpeg::JPEGEncoder;
+use image::{ColorType, ImageEncoder};
+use jpeg_decoder::PixelFormat;
 use std::error::Error;
 use std::fmt;
-use image;
-use image::{ColorType, ImageEncoder};
-use image::imageops;
-use jpeg_decoder;
-use jpeg_decoder::PixelFormat;
-use image::jpeg::JPEGEncoder;
-use exif::{Tag, In};
+use std::io::{self, Cursor, Read};
 use wasm_bindgen::JsValue;
 
 use wasm_bindgen::prelude::*;
@@ -29,12 +25,12 @@ cfg_if! {
 
 #[derive(Debug, Clone)]
 struct NailError {
-    message: String
+    message: String,
 }
 
 impl Error for NailError {
     fn description(&self) -> &str {
-       return &self.message.as_str()
+        &self.message.as_str()
     }
 
     fn cause(&self) -> Option<&(dyn Error)> {
@@ -53,7 +49,7 @@ fn get_orientation(input: &[u8]) -> Result<u32, Box<dyn Error>> {
     let data = exif::Reader::new().read_from_container(&mut io::Cursor::new(input))?;
     match data.get_field(exif::Tag::Orientation, exif::In::PRIMARY) {
         Some(res) => Ok(res.value.get_uint(0).unwrap_or(0)),
-        _ => Ok(0)
+        _ => Ok(0),
     }
 }
 
@@ -73,7 +69,7 @@ pub fn init_panic_hook() {
 #[derive(Serialize)]
 pub struct JSErr {
     message: String,
-    source: String
+    source: String,
 }
 
 // TODO: Can we use a macro for this boilerplate? Result<_, Box<dyn Error>> to Result<_, JsValue>
@@ -83,8 +79,9 @@ pub fn scale_and_orient(input: &[u8], max_dim: u32) -> Result<Vec<u8>, JsValue> 
         Ok(val) => Ok(val),
         Err(e) => Err(JsValue::from_serde(&JSErr {
             message: e.to_string(),
-            source: "nail-salon".into()
-        }).unwrap())
+            source: "nail-salon".into(),
+        })
+        .unwrap()),
     }
 }
 
@@ -94,7 +91,7 @@ fn _scale_and_orient(input: &[u8], max_dim: u32) -> Result<Vec<u8>, Box<dyn Erro
     let img = image::load_from_memory(&input)?;
 
     let thumb = img.thumbnail(max_dim, max_dim);
-    let thumb= match orientation {
+    let thumb = match orientation {
         // Reference: https://www.daveperrett.com/articles/2012/07/28/exif-orientation-handling-is-a-ghetto/
         // Reference: http://sylvana.net/jpegcrop/exif_orientation.html
         Ok(2) => thumb.fliph(),
@@ -104,12 +101,14 @@ fn _scale_and_orient(input: &[u8], max_dim: u32) -> Result<Vec<u8>, Box<dyn Erro
         Ok(6) => thumb.rotate90(),
         Ok(7) => thumb.rotate90().flipv(),
         Ok(8) => thumb.rotate270(),
-        _ => thumb
+        _ => thumb,
     };
 
     let mut out: Vec<u8> = Vec::new();
-    thumb.write_to(&mut out, ouput_fmt).expect("Failed to write output");
-    return Ok(out);
+    thumb
+        .write_to(&mut out, ouput_fmt)
+        .expect("Failed to write output");
+    Ok(out)
 }
 
 #[wasm_bindgen]
@@ -118,8 +117,9 @@ pub fn fast_scale_and_orient(input: &[u8], max_dim: u16) -> Result<Vec<u8>, JsVa
         Ok(val) => Ok(val),
         Err(e) => Err(JsValue::from_serde(&JSErr {
             message: e.to_string(),
-            source: "nail-salon".into()
-        }).unwrap())
+            source: "nail-salon".into(),
+        })
+        .unwrap()),
     }
 }
 
@@ -135,14 +135,20 @@ fn _fast_scale_and_orient(input: &[u8], max_dim: u16) -> Result<Vec<u8>, Box<dyn
     match metadata.pixel_format {
         PixelFormat::RGB24 => (),
         // We can add support for these CMYK, L8 in the future if needed.
-        _ => return Err(NailError{ message: "Unsupported format".into()}.into())
+        _ => {
+            return Err(NailError {
+                message: "Unsupported format".into(),
+            }
+            .into())
+        }
     };
 
     let img_buf = image::ImageBuffer::<image::Rgb<u8>, _>::from_raw(
         metadata.width as u32,
         metadata.height as u32,
-        decoder.decode()?
-    ).unwrap();
+        decoder.decode()?,
+    )
+    .unwrap();
 
     let img_buf = match orientation {
         2 => imageops::flip_horizontal(&img_buf),
@@ -152,14 +158,22 @@ fn _fast_scale_and_orient(input: &[u8], max_dim: u16) -> Result<Vec<u8>, Box<dyn
         6 => imageops::rotate90(&img_buf),
         7 => imageops::flip_vertical(&imageops::rotate90(&img_buf)),
         8 => imageops::rotate270(&img_buf),
-        _ => img_buf
+        _ => img_buf,
     };
 
     let out: Vec<u8> = Vec::new();
     let mut curs = io::Cursor::new(out);
 
-    let height = if orientation >= 5 { metadata.width } else {metadata.height };
-    let width = if orientation >= 5 { metadata.height } else {metadata.width };
+    let height = if orientation >= 5 {
+        metadata.width
+    } else {
+        metadata.height
+    };
+    let width = if orientation >= 5 {
+        metadata.height
+    } else {
+        metadata.width
+    };
 
     let enc = JPEGEncoder::new_with_quality(&mut curs, 90);
 
@@ -167,11 +181,11 @@ fn _fast_scale_and_orient(input: &[u8], max_dim: u16) -> Result<Vec<u8>, Box<dyn
         &img_buf.as_ref(),
         width as u32,
         height as u32,
-        ColorType::Rgb8)?;
+        ColorType::Rgb8,
+    )?;
 
-    return Ok(curs.into_inner());
+    Ok(curs.into_inner())
 }
-
 
 #[wasm_bindgen]
 pub fn embedded_thumbnail(input: &[u8]) -> Result<Vec<u8>, JsValue> {
@@ -179,30 +193,49 @@ pub fn embedded_thumbnail(input: &[u8]) -> Result<Vec<u8>, JsValue> {
         Ok(val) => Ok(val),
         Err(e) => Err(JsValue::from_serde(&JSErr {
             message: e.to_string(),
-            source: "nail-salon".into()
-        }).unwrap())
+            source: "nail-salon".into(),
+        })
+        .unwrap()),
     }
 }
 
 fn _embedded_thumbnail(input: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
     let mut curs = Cursor::new(input);
 
-    let meta = exif::Reader::new().read_from_container( &mut curs)?;
+    let meta = exif::Reader::new().read_from_container(&mut curs)?;
 
     let thumb_compression = match meta.get_field(Tag::Compression, In::THUMBNAIL) {
         Some(field) => field.value.get_uint(0),
-        _ => return Err(NailError {message: "No thumbnail".into()}.into())
+        _ => {
+            return Err(NailError {
+                message: "No thumbnail".into(),
+            }
+            .into())
+        }
     };
 
     match thumb_compression {
         Some(6) => (),
-        _ => return Err(NailError {message: "Unsupported thumbnail compression".into()}.into())
+        _ => {
+            return Err(NailError {
+                message: "Unsupported thumbnail compression".into(),
+            }
+            .into())
+        }
     };
-    let offset = meta.get_field(Tag::JPEGInterchangeFormat, In::THUMBNAIL).unwrap()
-        .value.get_uint(0).unwrap();
+    let offset = meta
+        .get_field(Tag::JPEGInterchangeFormat, In::THUMBNAIL)
+        .unwrap()
+        .value
+        .get_uint(0)
+        .unwrap();
 
-    let length = meta.get_field(Tag::JPEGInterchangeFormatLength, In::THUMBNAIL).unwrap()
-        .value.get_uint(0).unwrap();
+    let length = meta
+        .get_field(Tag::JPEGInterchangeFormatLength, In::THUMBNAIL)
+        .unwrap()
+        .value
+        .get_uint(0)
+        .unwrap();
 
     let mut curs = Cursor::new(input);
     curs.set_position((offset + 12) as u64);
@@ -210,8 +243,9 @@ fn _embedded_thumbnail(input: &[u8]) -> Result<Vec<u8>, Box<dyn Error>> {
     let mut out = Vec::new();
     let mut curs = curs.clone();
     curs.set_position((offset + 12) as u64);
-    curs.take((offset + 12 + length) as u64).read_to_end(&mut out)?;
-    return Ok(out);
+    curs.take((offset + 12 + length) as u64)
+        .read_to_end(&mut out)?;
+    Ok(out)
 }
 
 #[derive(Serialize)]
@@ -241,7 +275,7 @@ interface ImageMetadata {
 #[derive(Serialize)]
 pub struct ImageMetadata {
     primary: Vec<ExifItem>,
-    thumbnail: Vec<ExifItem>
+    thumbnail: Vec<ExifItem>,
 }
 
 /// Extract image metadata
@@ -249,10 +283,11 @@ pub struct ImageMetadata {
 pub fn exif_data(input: &[u8]) -> Result<JsValue, JsValue> {
     match _exif_data(input) {
         Ok(val) => Ok(JsValue::from_serde(&val).unwrap()),
-        Err(e) => return Err(JsValue::from_serde(&JSErr {
+        Err(e) => Err(JsValue::from_serde(&JSErr {
             message: e.to_string(),
-            source: "nail-salon".into()
-        }).unwrap())
+            source: "nail-salon".into(),
+        })
+        .unwrap()),
     }
 }
 
@@ -260,8 +295,8 @@ fn _exif_data(input: &[u8]) -> Result<ImageMetadata, Box<dyn Error>> {
     let mut curs = Cursor::new(input);
     let exif_data = exif::Reader::new().read_from_container(&mut curs)?;
 
-    let mut primary: Vec<ExifItem>  =  Vec::new();
-    let mut thumbnail: Vec<ExifItem>  =  Vec::new();
+    let mut primary: Vec<ExifItem> = Vec::new();
+    let mut thumbnail: Vec<ExifItem> = Vec::new();
 
     for field in exif_data.fields() {
         if Tag::MakerNote == field.tag {
@@ -272,7 +307,7 @@ fn _exif_data(input: &[u8]) -> Result<ImageMetadata, Box<dyn Error>> {
             ifd: field.ifd_num.index(),
             tag: field.tag.number(),
             desc: field.tag.description().unwrap_or("").to_string(),
-            value: field.display_value().to_string()
+            value: field.display_value().to_string(),
         };
 
         match field.ifd_num {
@@ -282,5 +317,5 @@ fn _exif_data(input: &[u8]) -> Result<ImageMetadata, Box<dyn Error>> {
         }
     }
 
-    return Ok(ImageMetadata{primary, thumbnail});
+    Ok(ImageMetadata { primary, thumbnail })
 }
