ddc5fdfc432e8d1150b89fc14ebacfbe841316dc
Chris Agerton <cagerton@gmail.com>
2020-11-11 05:37:20 -0800

    builder update

--- build.sh
+++ build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 set -eux
-[ -d "wasm" ] && rm -r wasm 
+
+export PATH=$PATH:$HOME/.cargo/bin
+
+[ -d "wasm" ] && rm -r wasm
 [ -d "dist" ] && rm -r dist
+
 cargo build --release --target wasm32-unknown-unknown
+
 wasm-bindgen --target nodejs --out-dir dist/wasm target/wasm32-unknown-unknown/release/nail_salon.wasm
+
 ./node_modules/.bin/tsc -p ./tsconfig.ci.json
+
 npm pack .
