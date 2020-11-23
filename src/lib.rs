7b788f3bc8a94a72baa08c32e5dd1aca2307f56f
Hugo Delahousse <hugo.delahousse@gmail.com>
2020-11-23 11:16:59 +0100

    Remove log binding

--- src/lib.rs
+++ src/lib.rs
@@ -38,14 +38,6 @@ interface ExposedFunctions {
 export type {ExposedFunctions};
 "#;
 
-#[wasm_bindgen]
-extern "C" {
-    // Use `js_namespace` here to bind `console.log(..)` instead of just
-    // `log(..)`
-    #[wasm_bindgen(js_namespace = console)]
-    fn log(s: &str);
-}
-
 #[derive(Serialize)]
 pub struct ImageInfo {
     format: String,
