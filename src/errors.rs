d653b945a9c4df2152406231081f69bbb8a839c8
Chris Agerton <cagerton@gmail.com>
2021-02-20 22:43:55 -0800

    Use macro for error enum

--- src/errors.rs
+++ src/errors.rs
@@ -1,46 +1,36 @@
 use wasm_bindgen::JsValue;
 
-#[derive(Debug)]
-pub enum MultiErr {
-    ImageError(image::error::ImageError),
-    ExifErr(exif::Error),
-    TryFromIntError(std::num::TryFromIntError),
-    GifEncodingError(gif::EncodingError),
-    GifDecodingError(gif::DecodingError),
-}
+macro_rules! error_enum {
+    (pub enum $name:ident {$(
+      $var:ident($t:ty)
+    ),+}) => {
+        #[derive(Debug)]
+        pub enum $name {
+          $($var($t),)+
+        }
 
-impl Into<JsValue> for MultiErr {
-    fn into(self) -> JsValue {
-        JsValue::from(format!("{:?}", self))
+        impl Into<JsValue> for $name {
+            fn into(self) -> JsValue {
+                JsValue::from(format!("{:?}", self))
+            }
+        }
+
+        $(
+            impl From<$t> for $name {
+                fn from(err: $t) -> Self {
+                    Self::$var(err)
+                }
+            }
+        )+
     }
 }
 
-impl From<image::error::ImageError> for MultiErr {
-    fn from(err: image::error::ImageError) -> MultiErr {
-        MultiErr::ImageError(err)
-    }
-}
-
-impl From<gif::EncodingError> for MultiErr {
-    fn from(err: gif::EncodingError) -> MultiErr {
-        MultiErr::GifEncodingError(err)
-    }
-}
-
-impl From<gif::DecodingError> for MultiErr {
-    fn from(err: gif::DecodingError) -> MultiErr {
-        MultiErr::GifDecodingError(err)
-    }
-}
-
-impl From<exif::Error> for MultiErr {
-    fn from(err: exif::Error) -> MultiErr {
-        MultiErr::ExifErr(err)
-    }
-}
-
-impl From<std::num::TryFromIntError> for MultiErr {
-    fn from(err: std::num::TryFromIntError) -> MultiErr {
-        MultiErr::TryFromIntError(err)
+error_enum! {
+    pub enum MultiErr {
+        ImageError(image::error::ImageError),
+        ExifErr(exif::Error),
+        TryFromIntError(std::num::TryFromIntError),
+        GifEncodingError(gif::EncodingError),
+        GifDecodingError(gif::DecodingError)
     }
 }
